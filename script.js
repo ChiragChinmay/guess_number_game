d5dbf04ce37d9deac4eda693a2c095734ade03ab
Chirag Chinmay <chiragchinmay2004@gmail.com>
2025-07-29 21:49:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 6) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    resultMessage("No number selected");
+  } else if (guess === secretNumber) {
+    resultMessage("No number selected");
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").style.backgroundColor = "red";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (score !== secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "too high" : "too low";
+
+      score -= 1;
+
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".score").textContent = 0;
+
+      resultMessage("you lost!");
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 6) + 1;
+
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".secretNumber").textContent = "?";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".secretNumber").style.width = "15rem";
+  document.querySelector(".secretNumber").style.backgroundColor = "";
+});
